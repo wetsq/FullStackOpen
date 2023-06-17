d312a0803877a22a67bb9a2438f9fff8e9df1a6c
Veeti <veeti.salminen@tuni.fi>
2023-06-18 00:10:13 +0300

    4.7

--- part4/tests/dummy.test.js
+++ part4/tests/dummy.test.js
@@ -1,5 +1,42 @@
 const listHelper = require('../utils/list_helper')
 
+const listWithOneBlog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }
+]
+const listWithBlogs = [
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+    },
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 10,
+        __v: 0
+    },
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 2,
+        __v: 0
+    }
+]
+
 test('dummy returns one', () => {
   const blogs = []
 
@@ -8,43 +45,6 @@ test('dummy returns one', () => {
 })
 
 describe('total likes', () => {
-    const listWithOneBlog = [
-      {
-        _id: '5a422aa71b54a676234d17f8',
-        title: 'Go To Statement Considered Harmful',
-        author: 'Edsger W. Dijkstra',
-        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-        likes: 5,
-        __v: 0
-      }
-    ]
-    const listWithBlogs = [
-        {
-            _id: '5a422aa71b54a676234d17f8',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-            likes: 5,
-            __v: 0
-        },
-        {
-            _id: '5a422aa71b54a676234d17f8',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-            likes: 10,
-            __v: 0
-        },
-        {
-            _id: '5a422aa71b54a676234d17f8',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-            likes: 2,
-            __v: 0
-        }
-    ]
-    
     test('of empty list is zero', () => {
         const result = listHelper.totalLikes([])
         expect(result).toBe(0)
@@ -60,3 +60,140 @@ describe('total likes', () => {
         expect(result).toBe(17)
     })
   })
+
+describe('most number of likes', () => {
+  test('of empty list is zero', () => {
+    const result = listHelper.favoriteBlog([])
+    expect(result).toBe(0)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    expect(result).toEqual(
+      {
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        likes: 5
+      }
+    )
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.favoriteBlog(listWithBlogs)
+    expect(result).toEqual(
+      {
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        likes: 10
+      }
+    )
+  })
+})
+
+describe('most number of blogs per author', () => {
+  const listWithBlogs = [
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+    },
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Obama',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 10,
+        __v: 0
+    },
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Obama',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 2,
+        __v: 0
+    }
+  ]
+
+  test('of empty list is zero', () => {
+    const result = listHelper.mostBlogs([])
+    expect(result).toBe(0)
+  })
+
+  test('when list has only one blog', () => {
+    const result = listHelper.mostBlogs(listWithOneBlog)
+    expect(result).toEqual(
+      {
+        author: 'Edsger W. Dijkstra',
+        blogs: 1
+      }
+    )
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.mostBlogs(listWithBlogs)
+    expect(result).toEqual(
+      {
+        author: 'Obama',
+        blogs: 2
+      }
+    )
+  })
+})
+
+describe('number of likes per author', () => {
+  const listWithBlogs = [
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+    },
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Obama',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 10,
+        __v: 0
+    },
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Obama',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 2,
+        __v: 0
+    }
+  ]
+
+  test('of empty list is zero', () => {
+    const result = listHelper.mostLikes([])
+    expect(result).toBe(0)
+  })
+
+  test('when list has only one blog', () => {
+    const result = listHelper.mostLikes(listWithOneBlog)
+    expect(result).toEqual(
+      {
+        author: 'Edsger W. Dijkstra',
+        likes: 5
+      }
+    )
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.mostLikes(listWithBlogs)
+    expect(result).toEqual(
+      {
+        author: 'Obama',
+        likes: 12
+      }
+    )
+  })
+})
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -10,7 +10,74 @@ const totalLikes = (blogs) => {
     return likes
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return 0
+    }
+    const mostLikes = Math.max(...blogs.map((blog) => blog.likes))
+    const favorite = blogs.find((blog) => blog.likes === mostLikes)
+    return(
+        {
+            title: favorite.title,
+            author: favorite.author,
+            likes: favorite.likes
+        }
+    )
+}
+
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0) {
+        return 0
+    }
+
+    const authors = []
+    for (i in blogs) {
+        if (authors.find(author => author.author === blogs[i].author)) {
+            const author = authors.find(author => author.author === blogs[i].author).author
+            authors[authors.map(a => a.author).indexOf(author)].blogs += 1
+        }
+        else {
+            authors.push(
+                {
+                    author: blogs[i].author,
+                    blogs: 1
+                }
+            )
+        }
+    }
+    const mostBlogs = Math.max(...authors.map((author) => author.blogs))
+    return authors.find((author) => author.blogs === mostBlogs)
+}
+
+const mostLikes = (blogs) => {
+    if (blogs.length === 0) {
+        return 0
+    }
+
+    const authors = []
+    for (i in blogs) {
+        if (authors.find(author => author.author === blogs[i].author)) {
+            const author = authors.find(author => author.author === blogs[i].author).author
+            console.log(author)
+            authors[authors.map(a => a.author).indexOf(author)].likes += Number(blogs[i].likes)
+        }
+        else {
+            authors.push(
+                {
+                    author: blogs[i].author,
+                    likes: blogs[i].likes
+                }
+            )
+        }
+    }
+    const mostLikes = Math.max(...authors.map((author) => author.likes))
+    return authors.find((author) => author.likes === mostLikes)
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
